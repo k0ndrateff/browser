466aaf5d3abae2c67614d2afc395206172498b58
Egor Kondratev <kondratevegor04@gmail.com>
2025-01-18 23:17:44 +0300

    feat: implement emojis

--- src/rendering/DisplayList.java
+++ src/rendering/DisplayList.java
@@ -4,7 +4,9 @@ import error.Logger;
 
 import java.awt.*;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.Objects;
 
 public class DisplayList {
     private String text;
@@ -18,11 +20,15 @@ public class DisplayList {
         int cursorX = position.x + hStep;
         int cursorY = position.y + vStep;
 
-        for (char c : text.toCharArray()) {
-            if (c == '\n') {
+        for (String c : this.splitText(text)) {
+            if (Objects.equals(c, "\n")) {
                 cursorX = position.x + hStep;
 
                 cursorY += vStep;
+            } else if (Emoji.isEmoji(c)) {
+                displayList.add(new Emoji(c, new Point(cursorX, cursorY)));
+
+                cursorX += hStep;
             } else {
                 displayList.add(new Character(String.valueOf(c), new Point(cursorX, cursorY)));
 
@@ -34,6 +40,15 @@ public class DisplayList {
                 cursorY += vStep;
             }
         }
+    }
+
+    private String[] splitText(String text) {
+        int[] codePoints = text.codePoints().toArray();
+
+
+        return Arrays.stream(codePoints)
+                .mapToObj(java.lang.Character::toString)
+                .toArray(String[]::new);
     }
 
     public Deque<RenderingComponent> getDisplayList() {
---
+++ src/rendering/Emoji.java
@@ -0,0 +1,83 @@
+package rendering;
+
+import error.Logger;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Emoji extends RenderingComponent {
+    private static final String EMOJI_DIR = "emojis";
+    private final String emoji;
+
+    private static final int SIZE = 16;
+
+    public Emoji(String emoji, Point position) {
+        this.emoji = emoji;
+        this.position = position;
+    }
+
+    public static boolean isEmoji(String ch) {
+        int[] codePoints = ch.codePoints().toArray();
+
+        Set<java.lang.Character.UnicodeBlock> emojiBlocks = Set.of(
+                java.lang.Character.UnicodeBlock.EMOTICONS,
+                java.lang.Character.UnicodeBlock.MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS,
+                java.lang.Character.UnicodeBlock.SUPPLEMENTAL_SYMBOLS_AND_PICTOGRAPHS,
+                java.lang.Character.UnicodeBlock.TRANSPORT_AND_MAP_SYMBOLS
+        );
+
+        boolean isEmoji = false;
+        for (int codePoint : codePoints) {
+            if (emojiBlocks.contains(java.lang.Character.UnicodeBlock.of(codePoint)) ||
+                    java.lang.Character.getType(codePoint) == java.lang.Character.OTHER_SYMBOL) {
+                isEmoji = true;
+            }
+        }
+
+        if (codePoints.length > 1 && codePoints[codePoints.length - 1] == 0xFE0F) {
+            isEmoji = true;
+        }
+
+        return isEmoji;
+    }
+    
+    private String getEmojiCode() {
+        int[] codePoints = emoji.codePoints().toArray();
+
+        return Arrays.stream(codePoints)
+                .mapToObj(cp -> String.format("%04X", cp)) // Convert to uppercase hex
+                .collect(Collectors.joining("-"));
+    }
+
+    private File getFile() {
+        return new File(EMOJI_DIR, getEmojiCode() + ".png");
+    }
+
+    @Override
+    public void paint(Graphics g, int scrollY) {
+        File file = getFile();
+
+        if (!file.exists()) {
+            Logger.verbose("Emoji from file " + file.getAbsolutePath() + "  not found! Rendering as text instead...");
+
+            g.drawString(String.valueOf(emoji), position.x, position.y - scrollY);
+        }
+        else {
+            try {
+                BufferedImage img = ImageIO.read(file);
+
+                // TODO: somehow calculate magic number 12, which is emoji image offset
+                g.drawImage(img, position.x, position.y - scrollY - 12, SIZE, SIZE, null);
+            } catch (IOException e) {
+                Logger.error(e);
+                g.drawString(String.valueOf(emoji), position.x, position.y - scrollY);
+            }
+        }
+    }
+}
