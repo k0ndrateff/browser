ebc415885e3fc5f1091c14864a75e2974418edb4
Egor Kondratev <kondratevegor04@gmail.com>
2025-01-24 21:43:15 +0300

    feat: implement html error-correcting parsing

--- src/document/HtmlParser.java
+++ src/document/HtmlParser.java
@@ -3,11 +3,11 @@ package document;
 import error.Logger;
 import rendering.TextRenderer;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class HtmlParser {
     private static final String[] SELF_CLOSING_TAGS = new String[] { "area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr" };
+    private static final String[] HEAD_TAGS = new String[] { "base", "basefont", "bgsound", "noscript", "link", "meta", "title", "style", "script" };
 
     private final HtmlDocument document;
     private final Stack<HtmlNode> unfinishedNodes;
@@ -81,6 +81,8 @@ public class HtmlParser {
     private void addTextNode(String text) {
         if (text.trim().isEmpty()) return;
 
+        this.insertImplicitTags(null);
+
         HtmlNode parent = this.unfinishedNodes.peek();
         HtmlNode textNode = new HtmlText(text, parent);
         parent.appendChildren(textNode);
@@ -88,6 +90,8 @@ public class HtmlParser {
 
     private void addElementNode(String tag) {
         if (tag.startsWith("!")) return;
+
+        this.insertImplicitTags(tag);
 
         if (tag.startsWith("/")) {
             if (this.unfinishedNodes.size() == 1) return;
@@ -117,6 +121,10 @@ public class HtmlParser {
     }
 
     private HtmlNode finishTree() {
+        if (this.unfinishedNodes.isEmpty()) {
+            this.insertImplicitTags(null);
+        }
+
         while (this.unfinishedNodes.size() > 1) {
             HtmlNode node = this.unfinishedNodes.pop();
             HtmlNode parent = this.unfinishedNodes.peek();
@@ -124,6 +132,34 @@ public class HtmlParser {
         }
 
         return this.unfinishedNodes.pop();
+    }
+
+    private void insertImplicitTags(String tag) {
+        final String[] headBodyHtmlTags = new String[] { "head", "body", "/html" };
+        final ArrayList<String> headTags = new ArrayList<>(List.of(HEAD_TAGS));
+        headTags.add("/head");
+
+        while (true) {
+            String[] openTags = this.unfinishedNodes.stream().map(HtmlNode::toString).toArray(String[]::new);
+
+            if (openTags.length == 0 && !Objects.equals(tag, "html")) {
+                this.addElementNode("html");
+            }
+            else if (openTags.length == 1 && openTags[0].equals("html") && !Arrays.asList(headBodyHtmlTags).contains(tag)) {
+                if (Arrays.asList(HEAD_TAGS).contains(tag)) {
+                    this.addElementNode("head");
+                }
+                else {
+                    this.addElementNode("body");
+                }
+            }
+            else if (openTags.length == 2 && openTags[0].equals("html") && openTags[1].equals("head") && !headTags.contains(tag)) {
+                this.addElementNode("/head");
+            }
+            else {
+                break;
+            }
+        }
     }
 
     private String resolveEntity(String entity) {
