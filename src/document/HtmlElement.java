8fc84e0dce594affbe857fde401b3482521e3820
Egor Kondratev <kondratevegor04@gmail.com>
2025-01-30 21:52:36 +0300

    feat: implement style inheritance

--- src/document/HtmlElement.java
+++ src/document/HtmlElement.java
@@ -81,6 +81,15 @@ public class HtmlElement extends HtmlNode {
     public void calculateStyle(ArrayList<CssBlock> rules) {
         Map<String, CssRule> style = new HashMap<>();
 
+        for (Map.Entry<String, String> entry : HtmlElement.getInheritableStyleProperties().entrySet()) {
+            if (this.parent != null && this.parent instanceof HtmlElement) {
+                style.put(entry.getKey(), ((HtmlElement) this.parent).getStyle().get(entry.getKey()));
+            }
+            else {
+                style.put(entry.getKey(), new CssRule(entry.getKey(), entry.getValue()));
+            }
+        }
+
         for (CssBlock cssBlock : rules) {
             if (!cssBlock.getSelector().matches(this)) continue;
 
@@ -95,6 +104,21 @@ public class HtmlElement extends HtmlNode {
 
         this.style = style;
 
+        if (style.containsKey("font-size") && style.get("font-size").getValue().endsWith("%")) {
+            String parentFontSize = "";
+
+            if (this.parent != null && this.parent instanceof HtmlElement) {
+                parentFontSize = ((HtmlElement) this.parent).getStyle().get("font-size").getValue();
+            }
+            else {
+                parentFontSize = HtmlElement.getInheritableStyleProperties().get("font-size");
+            }
+
+            float nodePercent = Float.parseFloat(this.style.get("font-size").getValue().replace("%", "")) / 100;
+            float parentPx = Float.parseFloat(parentFontSize.replace("px", ""));
+            this.style.put("font-size", new CssRule ("font-size", (nodePercent * parentPx) + "px"));
+        }
+
         for (HtmlNode child : this.getChildren()) {
             if (child instanceof HtmlElement) {
                 ((HtmlElement) child).calculateStyle(rules);
@@ -108,5 +132,16 @@ public class HtmlElement extends HtmlNode {
 
     public String toString() {
         return name;
+    }
+
+    private static Map<String, String> getInheritableStyleProperties() {
+        Map<String, String> props = new HashMap<>();
+
+        props.put("font-size", "16px");
+        props.put("font-style", "normal");
+        props.put("font-weight", "normal");
+        props.put("color", "black");
+
+        return props;
     }
 }
